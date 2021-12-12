8d2ad841de3c90a934dab43a0e6a1fb301efb619
barder60 <kaderate.yamamoto@hotmail.fr>
2021-12-13 00:00:13 +0100

    WIP(visual): add visual pygame example

---
+++ utils/spritesheet.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class SpriteSheet():
+    def __init__(self, image):
+        self.sheet = image
+
+    def get_image(self, frame, width, height, scale, colour):
+        if frame <= 4:
+            image = pygame.Surface((width, height), pygame.SRCALPHA).convert_alpha()
+            image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
+            image = pygame.transform.scale(image, (width * scale, height * scale))
+            image.set_colorkey(colour)
+            return image
+        else:
+
+            frame = frame % 4
+            print("on passe ici", frame)
+            image = pygame.Surface((width, height), pygame.SRCALPHA, 32).convert_alpha()
+            image.blit(self.sheet, (0, -125), ((frame * width), 0, width, height))
+            image = pygame.transform.scale(image, (width * scale, height * scale))
+            image.set_colorkey(colour)
+            return image
